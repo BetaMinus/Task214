e4c6020f3d9cee81e8eb174d31d277b660700df9
Leonid Simukov <pcjmeil@gmail.com>
2020-11-03 20:19:37 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Egg6.java
@@ -0,0 +1,19 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Egg6 {
+    private Deth8 deth;
+
+    @Autowired
+    public Egg6(Deth8 deth) {
+        this.deth = deth;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return ", в яйце иголка " + "" + deth.toString();
+    }
+}
